e3b90125e14ab15b0530ee13fa7bf6b82b242f26
Aldonovar <82477470+aldonovar@users.noreply.github.com>
2026-07-10 22:07:32 -0500

    privacy: remove founders section from home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { HeroSection } from "@/components/HeroSection";
 import { ServiciosGridSection } from "@/components/ServiciosGridSection";
 import { ProjectsShowcase } from "@/components/ProjectsShowcase";
 import { ProcesoSection } from "@/components/ProcesoSection";
-import { AboutSection } from "@/components/AboutSection";
 import { CotizaFormSection } from "@/components/CotizaFormSection";
 import { CTAFinal } from "@/components/CTAFinal";
 
@@ -11,12 +10,4 @@ export default function HomePage() {
   return (
     <>
       <HeroSection />
-      <ServiciosGridSection />
-      <ProjectsShowcase />
-      <ProcesoSection />
-      <AboutSection />
-      <CotizaFormSection />
-      <CTAFinal />
-    </>
-  );
-}
+     
